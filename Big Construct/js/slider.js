e58ed75d98869e116c8bf34e76da5fd4d82f7c26
Burduja Irina <125696292+bigconstruct@users.noreply.github.com>
2023-02-17 21:53:17 +0200

    Add files via upload

---
+++ Big Construct/js/slider.js
@@ -0,0 +1,57 @@
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 7000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 7000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
+
+
+//--------------slider-------------------//
+
+$('.partners-slider').slick({
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	dots: true,
+	arrows: true,
+	autoplay: true,
+	autoplaySpeed: 2000,
+	infinity: true
+
+});
